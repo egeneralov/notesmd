04558aedd111b90e912f3ef289eb4f59b3fee2f3
Your Name <you@example.com>
2017-03-31 01:36:44 +0300

    enchanting current state

--- generate.sh
+++ generate.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-	## Must have markdown deb packet installed in system
 
-#echo "Static site generator for md files;"
-echo ""
-dir=`pwd`;
+dpkg-query -l markdown > /dev/null || apt-get install markdown #must have
+clear; dir=`pwd`; echo "Static blog site generator for md files"; #init
+rm $dir/html/*.html > /dev/null 2>&1; rm $dir/html/blog/*.html > /dev/null 2>&1; ##clear current html state
+mkdir -p $dir/html/blog;mkdir -p $dir/blog; #check dir`s
+ls ~/mynotes || ln -s $dir/blog ~/mynotes; # fast link check
 
+echo "Old HTML removed";
+
+## Starting blog generator
 
 for file in `ls $dir/blog`; do
-	# Notify file name
-	echo "Generating blog/$file";
-
-    # Getting name of note
-	name=`cat $dir"/blog/$file" | head -n 1 | sed 's/\# //g'`
-	echo "	Name: $name"
+	echo "Generating blog/$file"; # Notify current file name
+	name=`cat $dir"/blog/$file" | head -n 1 | sed 's/\# //g'`; # getting note name
 	alias=`cat $dir"/blog/$file" | head -n 1 | sed 's/\# //g' | sed 's/ /_/g' | awk '{print tolower($0)}'`
-	echo "	Alias: $alias"
-
-
-    # Generating html from file
-	markdown $dir/blog/$file > $dir/html/blog/$alias.html
-    # Adding to menu
-	echo "<a href=\"/blog/$alias\">$name<\/a>"
+	markdown $dir/blog/$file > $dir/html/blog/$alias.html; # generating html from note
+	echo "<a class=\"menu\" href=\"/blog/$alias\">$name<\/a>" >> "$dir/menu.html"; # adding menu
 
 done
